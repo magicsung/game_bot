a1dcfb65fc16d4605632e80e58f8aededb6c331f
Jack Sung <magicsung@gmail.com>
2019-01-21 23:18:36 +0800

    install opencv

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
+'use strict'
+
+const _ = require('lodash');
 const robot = require('robotjs');
 
+const status = {
+  statusList: [
+    'start',
+    'choseMode',
+    'go',
+    'end1',
+    'end2',
+    'end3'
+  ],
+  latest: 'start',
+  get current() {
+    return this.latest;
+  },
+  next() {
+    let currentIndex = this.statusList.findIndex(item => item === this.latest);
+    if (currentIndex === this.statusList.length - 1) currentIndex = 0;
+    this.latest = this.statusList[currentIndex + 1];
+    return true;
+  }
+};
 
-/**
- * 判斷是否需要點擊
- * 1. 取得畫面上截圖
- * 2. 比對截圖相同比率 
- */
+console.log(status.current);
+for (let index = 0; index < 10; index++) {
+  status.next();
+  console.log(status.current);
+}
 
-/**
- * 操作流程
- * 1. 點擊任務
- * 2. 點擊單人狩獵
- * 3. 點擊準備完成
- * 4. 點擊出發
- * 5. 偵測是否結束
- * 6. 點擊確定
- * 7. 點擊確定
- * 8. 點擊OK
- *  */ 
+robot.setMouseDelay(2);
 
- /**
-  * TODO: 死亡處理
-  */
+// 1. 點擊任務
+if (status.current = status.statusList[0]) {
+  
+}
+
+// 2. 點擊單人狩獵
+// 3. 點擊準備完成
+// 4. 點擊出發
+// 5. 偵測是否結束
+// 6. 點擊確定
+// 7. 點擊確定
+// 8. 點擊OK
